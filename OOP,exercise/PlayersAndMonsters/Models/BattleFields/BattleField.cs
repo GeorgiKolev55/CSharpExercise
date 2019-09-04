0c26ceca32770d61b23314e8981447bc9099c2dc
GeorgiKolev55 <gkolev55@abv.bg>
2019-09-04 15:40:47 +0300

    .

---
+++ OOP,exercise/PlayersAndMonsters/Models/BattleFields/BattleField.cs
@@ -0,0 +1,37 @@
+﻿using PlayersAndMonsters.Models.BattleFields.Contracts;
+using PlayersAndMonsters.Models.Players;
+using PlayersAndMonsters.Models.Players.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.BattleFields
+{
+    public class BattleField : IBattleField
+    {
+        public void Fight(IPlayer attackPlayer, IPlayer enemyPlayer)
+        {
+            if (attackPlayer.IsDead==true||enemyPlayer.IsDead==true)
+            {
+                throw new ArgumentException("Player is dead!");
+            }
+            if (attackPlayer is Beginner)
+            {
+                attackPlayer.Health += 40;
+                foreach (var item in attackPlayer.CardRepository.Cards)
+                {
+                    item.DamagePoints += 30;
+                }
+            }
+            if (enemyPlayer is Beginner)
+            {
+                enemyPlayer.Health += 40;
+                foreach (var item in enemyPlayer.CardRepository.Cards)
+                {
+                    item.DamagePoints += 30;
+                }
+            }
+
+        }
+    }
+}
---
+++ OOP,exercise/PlayersAndMonsters/Models/Players/Player.cs
@@ -0,0 +1,58 @@
+﻿using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.Players
+{
+    public abstract class Player : IPlayer
+    {
+        private string username;
+        private int health;
+        public Player(ICardRepository cardRepository, string username, int health)
+        {
+            Username = username;
+            Health = health;
+            CardRepository = cardRepository;
+        }
+        public ICardRepository CardRepository { get; private set; }
+
+        public string Username
+        {
+            get { return this.username; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Player's username cannot be null or an empty string. ");
+                }
+                this.username = value;
+            }
+        }
+
+        public int Health
+        {
+            get { return this.health; }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Player's health bonus cannot be less than zero. ");
+                }
+                this.health = value;
+            }
+        }
+
+        public bool IsDead { get; private set; }
+
+        public void TakeDamage(int damagePoints)
+        {
+            if (Health-damagePoints<0)
+            {
+                throw new ArgumentException("Damage points cannot be less than zero.");
+            }
+            Health -= damagePoints;
+        }
+    }
+}
