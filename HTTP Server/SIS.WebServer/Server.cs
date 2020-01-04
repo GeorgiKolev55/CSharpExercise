e1fc80c26b75862c5b513c07574bd598fd9aa87e
GeorgiKolev55 <gkolev55@abv.bg>
2020-01-04 22:57:45 +0200

    HTTP Server.Practise

---
+++ HTTP Server/SIS.WebServer/Server.cs
@@ -0,0 +1,57 @@
+﻿using SIS.HTTP.Common;
+using SIS.WebServer.Routhing;
+using SIS.WebServer.Routhing.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SIS.WebServer
+{
+    public class Server
+    {
+        private const string LocalHostIpAddress = "127.0.0.1";
+
+        private readonly int port;
+
+        private readonly TcpListener tcpListener;
+
+        private IServerRouthingTable serverRoutingTable;
+
+        private bool isRunning;
+
+        public Server(int port, ServerRoutingTable serverRoutingTable)
+        {
+            CoreValidator.ThrowIfNull(serverRoutingTable, nameof(serverRoutingTable));
+
+            this.port = port;
+            this.serverRoutingTable = serverRoutingTable;
+
+            this.tcpListener = new TcpListener(IPAddress.Parse(LocalHostIpAddress), port);
+        }
+
+        private void Listen(Socket client)
+        {
+            var connectionHandler = new ConnectionHandler(client, this.serverRoutingTable);
+            connectionHandler.ProcessRequest();
+        }
+
+        public void Run()
+        {
+            this.tcpListener.Start();
+            this.isRunning = true;
+
+            Console.WriteLine($"Server started at http://{LocalHostIpAddress}:{this.port}");
+
+            while (this.isRunning)
+            {
+                Console.WriteLine("Waiting for client...");
+
+                var client = this.tcpListener.AcceptSocket();
+
+                this.Listen(client);
+            }
+        }
+    }
+}
---
+++ HTTP Server/webServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using SIS.HTTP.Enums;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Responses;
+using SIS.WebServer;
+using SIS.WebServer.Result;
+using SIS.WebServer.Routhing;
+using System;
+using System.Globalization;
+using System.Text;
+
+namespace webServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/", httpRequest
+                =>
+            {
+                return new HtmlResult("<h1>Hello World!<h1>", HttpResponseStatusCode.Ok);
+            });
+
+            Server server = new Server(8000, serverRoutingTable);
+            server.Run();
+        }
+    }
+}
