165f95b014a2e19d87e6fdc9853f72c09e0ac010
GeorgiKolev55 <gkolev55@abv.bg>
2021-07-20 05:56:49 +0300

    SoftServeDemo1

--- FindShortestPathInMaze/Maze/Node.cs
+++ FindShortestPathInMaze/Maze/Node.cs
@@ -6,13 +6,14 @@ namespace Maze
 {
     class Node
     {
-        //x and y are cordinates of a cell in the matrix(maze)
+        
         private readonly int rowCordinate;
         private readonly int colCordinate;
 
         //maintain the previous node for printing the final path
         private readonly Node previous;
-
+        
+        //x and y are cordinates of a cell in the matrix(maze)
         public Node(int x,int y,Node previous)
         {
             this.rowCordinate = x;
